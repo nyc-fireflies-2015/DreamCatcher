c943e237c9807fe811268c4a18d7976c208dfdf6
Albalenys <albalenysd@gmail.com>
2015-09-17 19:33:53 -0400

    add rspec tests for dreamers#new and dreamers#create controllers

--- spec/controllers/dreamers_controller_spec.rb
+++ spec/controllers/dreamers_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe DreamersController, type: :controller do
+  before(:all) do
+    @dreamer = FactoryGirl.create(:dreamer)
+    @dreamer_attributes = @dreamer.attributes
+  end
+
+  describe 'GET #new' do
+    it 'assigns a new dreamer' do
+      get :new
+      expect(assigns(:dreamer)).to be_a_new(Dreamer)
+    end
+  end
+
+  describe 'POST #create' do
+    context "valid attributes" do
+      it 'creates a new dreamer' do
+        post :create, dreamer: @dreamer_attributes
+        expect(Dreamer.last.username).to eq(@dreamer.username)
+        expect(Dreamer.last.email).to eq(@dreamer.email)
+      end
+
+      it "redirects to homepage with successful sign up" do
+        pending
+        post :create, dreamer: @dreamer_attributes
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "invalid attributes" do
+      it "does not create new dreamer with invalid attributes" do
+        expect{
+          post :create, dreamer: @dreamer_attributes.merge(username: nil)
+        }.to change(Dreamer, :count).by(0)
+      end
+
+      it "redirects to sign up page with unsuccessful sign up" do
+        post :create, dreamer: @dreamer_attributes.merge(username: nil)
+        expect(response).to redirect_to new_dreamer_path
+      end
+    end
+  end
 
 end
--- spec/factories/dreamer.rb
+++ spec/factories/dreamer.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :dreamer do
-    username { Faker::Internet.user_name }
+    username { Faker::Name.name }
+    email { Faker::Internet.email }
+    about { Faker::Lorem.paragraph }
+    recipe { Faker::Lorem.paragraph }
+    zipcode { Faker::Address.zip }
+    password "foobar"
+  end
+
+  factory :invalid_dreamer do
+    username { Faker::Name.name }
     email { Faker::Internet.email }
     about { Faker::Lorem.paragraph }
     recipe { Faker::Lorem.paragraph }
