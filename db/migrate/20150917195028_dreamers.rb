968b0d879a2113b4dadc206793b9326028016edb
ZedforZardoz <loweja01@gmail.com>
2015-09-17 15:57:23 -0400

    Add dreamers table to migrations

---
+++ db/migrate/20150917195028_dreamers.rb
@@ -0,0 +1,14 @@
+class Dreamers < ActiveRecord::Migration
+  def change
+    create_table :dreamers do |t|
+      t.string :username, null: false, limit: 64
+      t.string :password_digest, null: false
+      t.text :about, limit: 1024
+      t.text :recipe, limit: 2048
+      t.integer :level, default: 0
+      t.integer :zipcode, null: false, limit: 10
+      
+      t.timestamps null: false
+    end
+  end
+end
