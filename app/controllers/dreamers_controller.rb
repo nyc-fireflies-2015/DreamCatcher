1bb3e91092ed0b35a962bc9a5bee8b525c6dd486
Albalenys <albalenysd@gmail.com>
2015-09-17 18:30:56 -0400

    add create action for dreamer controller

--- app/controllers/dreamers_controller.rb
+++ app/controllers/dreamers_controller.rb
@@ -6,4 +6,21 @@ class DreamersController < ActionController::Base
   def new
     @dreamer = Dreamer.new
   end
+
+  def create
+    dreamer = Dreamer.new(dreamer_params)
+    if dreamer.save
+      # log_in(dreamer)
+      redirect_to dreamer_path(dreamer), notice: "Account Has Created!!"
+    else
+      flash[:error] = dreamer.errors.full_messages
+      redirect_to new_dreamer_path
+    end
+  end
+
+  private
+
+  def dreamer_params
+    params.require(:dreamer).permit(:username, :email, :password, :zipcode)
+  end
 end
