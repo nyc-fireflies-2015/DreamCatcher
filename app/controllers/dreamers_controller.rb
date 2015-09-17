62539718a0c5f97a584c9165d5c8f339c66b98f4
Albalenys <albalenysd@gmail.com>
2015-09-17 18:38:14 -0400

    add edit and update actions for dreamer controller

--- app/controllers/dreamers_controller.rb
+++ app/controllers/dreamers_controller.rb
@@ -12,9 +12,24 @@ class DreamersController < ActionController::Base
     if dreamer.save
       # log_in(dreamer)
       redirect_to dreamer_path(dreamer), notice: "Account Has Created!!"
+      #change above route to root_path
     else
       flash[:error] = dreamer.errors.full_messages
       redirect_to new_dreamer_path
+    end
+  end
+
+  def edit
+    @dreamer = Dreamer.find(params[:id])
+  end
+
+  def update
+    dreamer = current_user
+    if dreamer.update_attributes(dreamer_params)
+      redirect_to dreamer_path(dreamer)
+    else
+      flash[:error] = dreamer.errors.full_messages
+      redirect_to edit_user_path(dreamer)
     end
   end
 
