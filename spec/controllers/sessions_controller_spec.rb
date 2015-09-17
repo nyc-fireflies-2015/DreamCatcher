7faff9b1ea8483f2b503496f67e1be78340a48fc
aderend <d.dogrusoy@gmail.com>
2015-09-17 19:34:10 -0400

    sessions controller rspec tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  let(:dreamer){FactoryGirl.create: dreamer}
-  describe 'Login' do
-    it ''
+
+  context "new" do
+    it "is successful" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  context "#login" do
+    let(:dreamer) { FactoryGirl.create(:dreamer)}
+
+    it "redirects to root path if correct credentials" do
+      post :create, session:{username: dreamer.username, password: dreamer.password}
+      expect(response).to redirect_to root_path
+    end
+    it "renders login path with bad email" do
+      post :create, session:{username: "wrong@email.com", password: 'passwordddd'}
+      expect(response).to render_template('new')
+    end
+    it "renders login path with bad password" do
+      post :create, session:{username: dreamer.username, password: "wrong password"}
+      expect(response).to redirect_to root_path
+    end
   end
 end
