f79fb5cadb3914c8d19cc2cd958d478fd1c3b7de
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-17 18:19:15 -0400

    add associations and validations to dreamer model spec

---
+++ spec/models/dreamer_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe Dreamer do
+  context "associations" do
+    it "should have many dreams" do
+      should have_many :dreams
+    end
+    it "should have many favorites through foreign key fan_id" do
+      should have_many(:favorites).with_foreign_key(:fan_id)
+    end
+  end
+  context "validations" do
+    it "validates format of email" do
+      should validate_email_format_of(:email).with_message("Please enter valid email") 
+    end
+    it "validates presence of username" do
+      should validate_presence_of :username
+    end
+    it "validates presence of email" do
+      should validate_presence_of :email
+    end
+    it "validates presence of zipcode" do
+      should validate_presence_of :zipcode
+    end
+  end
+end
