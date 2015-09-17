81470c38a4f6f93a25d49184465e3cc858d02445
ZedforZardoz <loweja01@gmail.com>
2015-09-17 16:23:36 -0400

    Add column for email to dreamers

---
+++ app/models/Dreamer.rb
@@ -0,0 +1,7 @@
+class Dreamer < ActiveRecord::Base
+  has_many :dreams
+  has_many :favorites, foreign_key: :fan_id
+  validates_length_of :password, minimum: 6, too_short: "please enter at least %d character"
+  validates :username
+
+end
