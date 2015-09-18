3ffff9c76cdd7b75a111ddda311d6987e6bcfce9
Albalenys <albalenysd@gmail.com>
2015-09-18 14:50:10 -0400

    change dreamer new route to 'signup'

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root 'dreams#index'
+
   resources :dreams
   resources :favorites, only: [:create, :destroy]
 
-  resources :dreamers, except: [:index, :show, :destroy]
+  resources :dreamers, except: [:index, :show, :new, :destroy]
   get '/profile/:id' => 'dreamers#show', as: 'profile'
-
-  root 'dreams#index'
+  get '/signup' => 'dreamers#new', as: 'signup'
 
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
