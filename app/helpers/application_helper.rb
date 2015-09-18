92472a4331ef1d03fb615b8b9859b079d1b7c411
Ramon Ramos III <email": null>
2015-09-17 20:39:11 -0400

    Move current_user method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_user
+    Dreamer.find_by(id: session[:user_id])
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,2 @@
 module SessionsHelper
-  def current_user
-    Dreamer.find_by(id: session[:user_id])
-  end
 end
