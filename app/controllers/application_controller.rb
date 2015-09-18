1a273d17ed4577d73efb78428d468125618382fa
Ramon Ramos III <email": null>
2015-09-17 21:42:26 -0400

    Correct current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= Dreamer.find_by(id: session[:user_id])
   end
 
 end
