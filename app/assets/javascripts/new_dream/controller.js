d81cde95e39e7fe08189017541152dc2c3cf8c52
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-18 15:45:34 -0400

    Add new dream controller js

---
+++ app/assets/javascripts/new_dream/controller.js
@@ -0,0 +1,7 @@
+NewDreamController = function(){
+  // this.view = new NewDreamView
+}
+
+NewDreamController.prototype.getDreamForm = function(eventTarget){
+        console.log(eventTarget);
+};
--- app/assets/javascripts/new_dream/runner.js
+++ app/assets/javascripts/new_dream/runner.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
+  var ctrl = new NewDreamController();
   Binder.bind({event: "click", 
               selector: "[data-link=new-dream]", 
-              callback: function(){alert("woo");}})
+              callback: ctrl.getDreamForm })
 });
