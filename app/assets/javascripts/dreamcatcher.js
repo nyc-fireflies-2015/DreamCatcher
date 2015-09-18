c30110d6696bdf669f351a6bc0274f3359c8628e
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-18 19:13:15 -0400

    Debug Obj.fromFrom

--- app/assets/javascripts/dreamcatcher.js
+++ app/assets/javascripts/dreamcatcher.js
@@ -7,17 +7,11 @@ Binder.bind = function(args){
 };
 
 var Obj = {};
-Obj.fromForm = function(args){
+Obj.fromForm = function(form){
    var formData = {};
-   if(args.type !== undefined){
-    formData.type = $(args.form).attr("method");
-    }
-   if(args.url !== undefined){
-   formData.url = $(args.form).attr("action");
-    }
-   if(args.data !== undefined){
-   formData.data = $(args.form).serialize();
-    }
+   formData.type = $(form).attr("method");
+   formData.url = $(form).attr("action");
+   formData.data = $(form).serialize();
    return formData;
 };
 Obj.fromLink = function(link){
