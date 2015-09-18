0060bd72ab9786402987a4f6994f68ddaa70e121
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-18 18:05:09 -0400

    Add post path to new dream contoller

--- app/assets/javascripts/new_dream/controller.js
+++ app/assets/javascripts/new_dream/controller.js
@@ -1,5 +1,9 @@
 NewDreamController = function(){}
 NewDreamController.prototype.getDreamForm = function(eventTarget){
       DreamCatcher.send({data: Obj.fromLink(eventTarget), 
-                        callback: NewDreamView.show });
+                        callback: NewDreamView.showDream });
+};
+NewDreamController.prototype.postDreamForm = function(eventTarget){
+      DreamCatcher.send({data: Obj.fromForm(eventTarget),
+                        callback: NewDreamView.hideDream })
 };
