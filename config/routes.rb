9339c312e199138e86db5a8998228c73152807b1
Albalenys <albalenysd@gmail.com>
2015-09-18 14:47:04 -0400

    change show route for dreamers to 'profile'

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :dreamers, except: [:index, :destroy]
   resources :dreams
   resources :favorites, only: [:create, :destroy]
+
+  resources :dreamers, except: [:index, :show, :destroy]
+  get '/profile/:id' => 'dreamers#show', as: 'profile'
 
   root 'dreams#index'
 
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-
 end
