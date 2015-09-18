5ce720f84fc3a36b3bdcba3805d5eca57ef9bb0e
Ramon Ramos III <email": null>
2015-09-18 14:47:21 -0400

    Fix rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,13 +22,14 @@ require 'shoulda/matchers'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include LoginMacros, :type => :controller
   config.include Warden::Test::Helpers
   config.before :suite do
     Warden.test_mode!
