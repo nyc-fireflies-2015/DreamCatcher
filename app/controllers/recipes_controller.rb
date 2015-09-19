c6eef1738103acea18fe3bbf1cc140be3ebdb4fa
Ramon Ramos III <email": null>
2015-09-18 20:41:03 -0400

    Fix update method in Recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,25 @@ class RecipesController < ApplicationController
   def show
     @dreamer = Dreamer.find(params[:id])
   end
+
+  def edit
+    @dreamer = Dreamer.find(params[:id])
+  end
+
+  def update
+    @dreamer = Dreamer.find(params[:id])
+    @dreamer.update_attributes(recipe_params)
+    if @dreamer.save
+      redirect_to recipe_path(@dreamer)
+    else
+      flash[:error] = "Something went wrong."
+      redirect_to edit_recipe_path(@dreamer)
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:dreamer).permit(:recipe, :password)
+  end
 end
