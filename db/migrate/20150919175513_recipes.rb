c8ed0e8c5b7564b3327598616e0c5e34fae43ba5
Ramon Ramos III <email": null>
2015-09-19 14:37:02 -0400

    Fix recipes and steps table references

--- db/migrate/20150919175513_recipes.rb
+++ db/migrate/20150919175513_recipes.rb
@@ -2,6 +2,7 @@ class Recipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name, null: false, limit: 512
+      t.references :creator
 
       t.timestamps null: false
     end
--- db/migrate/20150919175529_steps.rb
+++ db/migrate/20150919175529_steps.rb
@@ -2,6 +2,7 @@ class Steps < ActiveRecord::Migration
   def change
     create_table :steps do |t|
       t.string :description, null: false, limit: 2048
+      t.references :recipe
 
       t.timestamps null: false
     end
