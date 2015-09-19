87a45dc60596ecf06d8c11cb991679d24b6c591b
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-19 15:25:09 -0400

    add recipe and step to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Dreamer.delete_all
+Recipe.delete_all
+Dream.delete_all
+Step.delete_all
+
 10.times do
   d = FactoryGirl.create(:dreamer)
+  r = Recipe.create(FactoryGirl.attributes_for(:recipe).merge(creator: d))
+  4.times do
+    r.steps.create(FactoryGirl.attributes_for(:step))
+  end
   4.times do
     ds = d.dreams.create(FactoryGirl.attributes_for(:dream))
     5.times do
