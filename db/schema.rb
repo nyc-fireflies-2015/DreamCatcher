8531eb08b41d0b69a454df78023c5b858628f7ed
Ramon Ramos III <email": null>
2015-09-19 14:04:46 -0400

    Add recipe and step tables to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917210128) do
+ActiveRecord::Schema.define(version: 20150919175529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,18 @@ ActiveRecord::Schema.define(version: 20150917210128) do
     t.integer  "fan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name",       limit: 512, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.string   "description", limit: 2048, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
