01c23c9e9e1a39534955512257903be394dd624e
Ramon Ramos III <email": null>
2015-09-18 20:41:41 -0400

    Remove password restriction

--- app/models/dreamer.rb
+++ app/models/dreamer.rb
@@ -3,6 +3,6 @@ class Dreamer < ActiveRecord::Base
   validates_email_format_of :email, message: "Please enter valid email"
   has_many :dreams
   has_many :favorites, foreign_key: :fan_id
-  validates_length_of :password, minimum: 6, too_short: "please enter at least %d character"
+  validates :password, length: {minimum: 6}
   validates_presence_of :email, :username, :zipcode
 end
