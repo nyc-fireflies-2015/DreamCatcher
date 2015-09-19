8386738daad5145b6e47ca8c3d00d99f477cc2af
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-19 16:33:39 -0400

    Dreamer gets default recipe

---
+++ db/migrate/20150919201119_add_default_column_to_items.rb
@@ -0,0 +1,5 @@
+class AddDefaultColumnToItems < ActiveRecord::Migration
+  def change
+    add_column :steps, :default, :boolean, default: false 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919220855) do
-
+ActiveRecord::Schema.define(version: 20150919201119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,10 +57,11 @@ ActiveRecord::Schema.define(version: 20150919220855) do
   end
 
   create_table "steps", force: :cascade do |t|
-    t.string   "description", limit: 2048, null: false
+    t.string   "description", limit: 2048,                 null: false
     t.integer  "recipe_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.boolean  "default",                  default: false
   end
 
 end
