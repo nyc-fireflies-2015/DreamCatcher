38d172ed92edc7a67b4b2a581c3e8bd9d14737d5
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-19 17:01:54 -0400

    create populate steps rake task

---
+++ db/populate.rb
@@ -0,0 +1,12 @@
+def populate_steps
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+  Step.create(description: Faker::Lorem.sentence, default: true)
+end
