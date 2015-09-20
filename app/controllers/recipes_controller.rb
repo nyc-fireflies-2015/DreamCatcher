7809f2076fae7c8f3b59b032ea9df99fcfe48c0a
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-19 21:04:50 -0400

    Clean up recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,13 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :create_step]
-
+  before_action :find_step, only: [:update, :add_step, :remove_step]
   def update
-    @step = Step.find(params[:id])
     @step.update_attributes(step_params)
     unless @step.save
       flash[:error] = @step.errors.full_messages
     end
     respond_to do |format|
       format.html { redirect_to :back }
-      format.js 
+      format.js { render "save_step.js.erb"}
     end
   end
 
@@ -17,49 +15,30 @@ class RecipesController < ApplicationController
     @step = Step.new
     respond_to do |format|
       format.html { render partial: "new_step", locals: {step: @step} }
-      format.js {render "new_step.js.erb" }
+      format.js { render "new_step.js.erb" }
+    end
+  end
+
+  def create_step
+    @step = @recipe.steps.build(step_params.merge(creator: current_dreamer))
+    unless @step.save
+      flash[:error] = @step.errors.full_messages
+    end
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render "save_step.js.erb" }
     end
   end
 
   def add_step
-    current_dreamer.recipe.steps << Step.find(params[:id])
-  end
-
-  def create_step
-    @step = @recipe.steps.build(step_params.merge(creator: current_dreamer))
-    unless @step.save
-      flash[:error] = @step.errors.full_messages
-    end
+    current_dreamer.recipe.steps << @step
     respond_to do |format|
       format.html { redirect_to :back }
-    end
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js 
+      format.js { render "step.js.erb" }
     end
   end
 
-  def cancel_step
-    @step = Step.find(params[:id])
-    if @step.creator == current_dreamer
-      @step.destroy
-    else
-      current_dreamer.recipe.steps.delete(@step)
-    end
-  end
-
-  def create_step
-    @step = @recipe.steps.build(step_params.merge(creator: current_dreamer))
-    unless @step.save
-      flash[:error] = @step.errors.full_messages
-    end
-    respond_to do |format|
-      format.html { redirect_to :back }
-    end
-  end
-
-  def cancel_step
-    @step = Step.find(params[:id])
+  def remove_step
     if @step.creator == current_dreamer
       @step.destroy
     else
@@ -73,7 +52,7 @@ class RecipesController < ApplicationController
     params.require(:step).permit(:description)
   end
 
-  def find_recipe
-     @recipe = Recipe.find(params[:id])
+  def find_step
+     @recipe = Step.find(params[:id])
   end
 end
