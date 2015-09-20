ebe8a1b204a662a40d2759db84fae8a46503b581
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-20 18:50:08 -0400

    Add dream association to comments in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,13 @@ class CommentsController < ApplicationController
   end
 
   def create
-    comment = Comment.new(comment_params.merge(dreamer: current_dreamer))
+    dream = Dream.find(comment_params[:dream_id])
+    comment = Comment.new(comment_params.merge(dreamer: current_dreamer, dream: dream))
     if comment.save
-      redirect_to dream_path(comment_params[:dream_id])
+      redirect_to dream_path(dream)
     else
       flash[:error] = comment.errors.full_messages
-      redirect_to dream_path(comment_params[:dream_id])
+      redirect_to dream_path(dream)
     end
   end
 
