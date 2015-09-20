bbf505d2065ea9c34a5cfe5d28b271117afef3d6
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-19 21:46:45 -0400

    Add creator column to step

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,6 @@
 class Step < ActiveRecord::Base
   has_and_belongs_to_many :recipes
+  belongs_to :creator, class_name: "Dreamer"
   def self.default
     where(default: true)
   end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -5,5 +5,8 @@ describe Step do
     it "has many recipes" do
       should have_and_belong_to_many(:recipes)
     end
+    it "belongs to a creator (class name Dreamer)" do
+      should belong_to(:creator).class_name(Dreamer)
+    end
   end
 end
