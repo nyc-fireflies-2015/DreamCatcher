6363945a513b1f5372ce8350af2e9a3b1d33b362
arjis02 <rj.ramos@gmail.com>
2015-09-20 01:13:35 -0400

    Add zipcode format validation

--- app/models/dreamer.rb
+++ app/models/dreamer.rb
@@ -5,5 +5,8 @@ class Dreamer < ActiveRecord::Base
   has_many :favorites, foreign_key: :fan_id
   has_one :recipe, foreign_key: :creator_id
   validates :password, length: {minimum: 6}
+  validates_format_of :zip_code,
+                  with: /\A\d{5}-\d{4}|\A\d{5}\z/,
+                  message: "should be 12345 or 12345-1234"
   validates_presence_of :email, :username, :zipcode
 end
