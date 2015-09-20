ed9071ee1c3ab5b18a753bf451e5519fcddc7428
Albalenys <albalenysd@gmail.com>
2015-09-20 12:11:14 -0400

    add new comment form to dreams show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    comment = Comment.new(comment_params.merge(dreamer_id: current_dreamer)
+    if comment.save
+      redirect_to dream_path
+    else
+      flash[:error] = comment.errors.full_messages
+      redirect_to new_comment_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :dreamer_id, :dream_id)
+  end
+
 end
