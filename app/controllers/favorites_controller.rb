187aecb0646eacb279d105ff0d53f1da7e0ff86d
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-20 17:16:40 -0400

    Add js logic to favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.create(dream: @dream, fan: current_dreamer)
     respond_to do |format|
-      format.js { render "favorite.js.erb" }
+      format.js { render "favorites/favorite.js.erb" }
     end
   end
 
   def destroy
     @dream.favorite_by(current_dreamer).destroy
     respond_to do |format|
-      format.js { render "un_favorite.js.erb" }
+      format.js { render "favorites/un_favorite.js.erb" }
     end
   end
 
