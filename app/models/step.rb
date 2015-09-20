cb21e50dc74061afcb695fab3333729577fe2ac5
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-19 23:45:09 -0400

    Add timestamp method to step

--- app/models/step.rb
+++ app/models/step.rb
@@ -7,4 +7,21 @@ class Step < ActiveRecord::Base
   def self.top
     order('recipes_count DESC').limit(10)
   end
+
+  def timestamp
+    seconds = (Time.now - self.created_at).to_i
+    minutes = seconds/60
+    hours = minutes/60
+    days = hours/24
+
+    if minutes == 0
+      return "#{seconds} seconds"
+    elsif hours == 0
+      return "#{minutes} minutes"
+    elsif days == 0
+      return "#{hours} hours"
+    else
+      return "#{days} days"
+    end
+  end
 end
