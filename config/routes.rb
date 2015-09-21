aaf1bf14587dc9079c88cb303158c272363c2fc6
Ramon Ramos III & James Lowenthal <pair+arjis02.jamesanthonylow@devbootcamp.com>
2015-09-20 21:38:28 -0400

    rewrite routes to utilize step resources

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,14 @@ Rails.application.routes.draw do
   post '/sessions' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  get '/steps/new' => 'recipes#new_step', as: 'new_step'
-  post '/recipes/steps' => 'recipes#create_step', as: 'create_step'
-  get '/recipes/:id' => 'recipes#show', as: 'recipe'
-  post '/steps/:id' => 'recipes#add_step', as: 'add_step'
-  get '/steps/:id/edit' => 'recipes#edit_step', as: 'edit_step'
-  patch '/steps/:id' => 'recipes#update', as: 'update_recipe_step'
-  patch '/steps/:id/remove' => 'recipes#remove_step', as: 'remove_step'
+  resources :step
+  # get '/steps/new' => 'recipes#new_step', as: 'new_step'
+  # post '/recipes/steps' => 'recipes#create_step', as: 'create_step'
+  # get '/recipes/:id' => 'recipes#show', as: 'recipe'
+  # post '/steps/:id' => 'recipes#add_step', as: 'add_step'
+  # get '/steps/:id/edit' => 'recipes#edit_step', as: 'edit_step'
+  # patch '/steps/:id' => 'recipes#update', as: 'update_recipe_step'
+  # patch '/steps/:id/remove' => 'recipes#remove_step', as: 'remove_step'
 
   get '/twilio' => 'twilio#index'
   post '/send_sms' => 'twilio#send_sms'
