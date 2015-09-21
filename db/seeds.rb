9789fcf4f8576bd8e549c855f520bfabf8fbf820
Ramon Ramos III <email": null>
2015-09-21 15:14:49 -0400

    Modify seed to set default recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,16 @@ Recipe.delete_all
 Dream.delete_all
 Step.delete_all
 
+Step.create(description: "Upon awakening, remain absolutely still while you recall the essential elements of the dream and log it in DreamCatcher.", default: true)
+Step.create(description: "Wear a digital watch and read the words or numbers on the watch. If the characters are normal, stable, and sensible, then you probably aren't dreaming.", default: true)
+Step.create(description: "Look for dream signs (elements that indicate that you are dreaming).", default: true)
+Step.create(description: "Awaken one hour earlier than usual, stay awake for 30 to 60 minutes, then go back to sleep.", default: true)
+Step.create(description: "Imagine your surroundings are a dream. Intently imagine that what you are seeing, hearing, smelling, and feeling is all a dream.", default: true)
+Step.create(description: "Try to fly! If it works, you're most likely dreaming", default: true)
+
 10.times do
   c = FactoryGirl.create(:dreamer)
-  4.times do
-    s = Step.create(FactoryGirl.attributes_for(:step).merge(creator: c))
-      10.times do
-        s.dreamers << FactoryGirl.create(:dreamer)
-      end
-  end
+  c.steps << Step.all
   4.times do
     ds = c.dreams.create(FactoryGirl.attributes_for(:dream))
     5.times do
