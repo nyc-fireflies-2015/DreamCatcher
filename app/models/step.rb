49f823f5d201e81204be64f25bd84bbd3c417372
Ramon Ramos III & James Lowenthal <pair+arjis02.jamesanthonylow@devbootcamp.com>
2015-09-20 21:18:50 -0400

    Update models and spec for new relationship

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,11 +1,12 @@
 class Step < ActiveRecord::Base
-  has_and_belongs_to_many :recipes
+  has_many :recipes
+  has_many :dreamers, through: :recipes
   belongs_to :creator, class_name: "Dreamer"
 
   def self.default
     where(default: true)
   end
-  
+
   def self.top
     order('recipes_count DESC').limit(10)
   end
