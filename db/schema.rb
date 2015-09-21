0fc01283cd76428ca410a2a2d19026397b02a910
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-20 23:21:27 -0400

    update _profile view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921010715) do
+ActiveRecord::Schema.define(version: 20150921031735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150921010715) do
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
     t.string   "title",                  limit: 128
+    t.integer  "favorites_count"
   end
 
   create_table "favorites", force: :cascade do |t|
