1f30b546cb17cdd460cb2eddb8eced1c0bf37502
Ramon Ramos III <email": null>
2015-09-21 17:45:20 -0400

    Remove timestamp method

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -15,23 +15,6 @@ class Dream < ActiveRecord::Base
     favorites.where(fan: dreamer).any?
   end
 
-  def timestamp
-    seconds = (Time.now - self.created_at).to_i
-    minutes = seconds/60
-    hours = minutes/60
-    days = hours/24
-
-    if minutes == 0
-      return "#{seconds} seconds"
-    elsif hours == 0
-      return "#{minutes} minutes"
-    elsif days == 0
-      return "#{hours} hours"
-    else
-      return "#{days} days"
-    end
-  end
-
   def calculate_lucidity
     lucidity_rating = 0
     lucidity_rating += 1 if self.consciousness_clarity?
