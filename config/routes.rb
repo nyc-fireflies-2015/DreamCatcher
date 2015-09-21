e40fbbe58b92e7516e7ca71bb16fe94acc49aeea
Ramon Ramos III & James Lowenthal <pair+arjis02.jamesanthonylow@devbootcamp.com>
2015-09-20 21:52:24 -0400

    extract form into partial

--- config/routes.rb
+++ config/routes.rb
@@ -17,13 +17,14 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   resources :step
+  post '/steps/:id' => 'steps#add_step', as: 'add_step'
+  patch '/steps/:id/remove' => 'recipes#remove_step', as: 'remove_step'
+
   # get '/steps/new' => 'recipes#new_step', as: 'new_step'
   # post '/recipes/steps' => 'recipes#create_step', as: 'create_step'
   # get '/recipes/:id' => 'recipes#show', as: 'recipe'
-  # post '/steps/:id' => 'recipes#add_step', as: 'add_step'
   # get '/steps/:id/edit' => 'recipes#edit_step', as: 'edit_step'
   # patch '/steps/:id' => 'recipes#update', as: 'update_recipe_step'
-  # patch '/steps/:id/remove' => 'recipes#remove_step', as: 'remove_step'
 
   get '/twilio' => 'twilio#index'
   post '/send_sms' => 'twilio#send_sms'
