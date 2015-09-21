76f7d04860f7a5e245fdecf41439e621aa615656
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-21 09:47:50 -0400

    fix route orderr

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
   post '/sessions' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :steps, except: [:destroy, :index]
   get 'dreamers/:id/steps' => 'steps#index', as: 'steps'
+  resources :steps, except: [:destroy, :index]
   post '/steps/:id' => 'steps#add_step', as: 'add_step'
   patch '/steps/:id/remove' => 'steps#remove_step', as: 'remove_step'
 
