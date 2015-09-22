da516ab243b099a180612b09dc8204d3cf142867
Albalenys <albalenysd@gmail.com>
2015-09-22 10:12:50 -0400

    add 'will_paginate' gem

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,7 +4,7 @@ class ConversationsController < ApplicationController
   before_action :get_conversation, except: [:index]
 
   def index
-    @conversations = @mailbox.inbox
+    @conversations = @mailbox.inbox.paginate(page: params[:page], per_page: 10)
   end
 
   private
