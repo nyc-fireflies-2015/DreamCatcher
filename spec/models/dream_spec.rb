aa44c9f9b4e92e2a3b5f0c32dc07321932b7d0df
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-22 10:22:00 -0400

    Update dream spec

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -8,12 +8,15 @@ describe Dream do
     it "should have many favorites" do
       should have_many :favorites
     end
+    it "should have many hashtags" do
+      should have_many :hashtags
+    end
   end
   context 'validations' do
     it "validates the presence of story" do
       should validate_presence_of :story
     end
-    it "valides the presence of title" do
+    it "validates the presence of title" do
       should validate_presence_of :title
     end
   end
