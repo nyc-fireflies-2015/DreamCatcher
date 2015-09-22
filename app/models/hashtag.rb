7a991f965ce14bfde85caf88648c9fc67c2177f8
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-22 10:45:50 -0400

    Add parsing methdo to hashtag model

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,9 @@
 class Hashtag < ActiveRecord::Base
   has_many :taggings
   has_many :dreams, through: :taggings
+  def self.parse(hashtag_string)
+    hashtag_string.split(", ").each_with_object([]) do |name, hashtags|
+      hashtags << find_or_create_by(name: name)
+    end
+  end
 end
