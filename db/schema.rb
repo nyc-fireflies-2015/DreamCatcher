64eb4a78efe94aa1e5ef90128068f8a8a7bccc50
Albalenys <albalenysd@gmail.com>
2015-09-22 14:58:26 -0400

    remove zipcode from dreamers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922184419) do
+ActiveRecord::Schema.define(version: 20150922185618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20150922184419) do
     t.string   "name"
     t.text     "about"
     t.integer  "level",                      default: 0
-    t.string   "zipcode"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "email"
