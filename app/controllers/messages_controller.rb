d6f027578bf9d2c0dce4e4e3ca293cd33c39ffaf
Albalenys <albalenysd@gmail.com>
2015-09-22 11:53:30 -0400

    add 'send message' to dreamer profiles

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_dreamer
 
   def new
+    @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
   end
 
   def create
