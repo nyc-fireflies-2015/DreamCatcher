3718bbc51ec123671d7c8d93345e471a5a84cb70
aderend <d.dogrusoy@gmail.com>
2015-09-22 19:45:23 -0400

    hide twilio account info

--- app/models/text_worker.rb
+++ app/models/text_worker.rb
@@ -18,7 +18,7 @@ class TextWorker
     message = 'This is a test'
     account_sid = 'AC40f31b05ec16c2c813436c84ada30a6f'
     auth_token = '6866e1d92472d67960fe7173589eac33'
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
     @message = @client.account.messages.create({to: "+1"+"#{number}",
                                                   from: "+13157074332",
                                                   body: "#{message}" })
