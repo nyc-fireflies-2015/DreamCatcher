7a7b7a1090b71d6678bf583709561404f3dca671
Albalenys <albalenysd@gmail.com>
2015-09-21 20:28:32 -0400

    add messages controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,14 +1,19 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_dreamer
   before_action :get_mailbox
+  before_action :get_conversation, except: [:index]
 
   def index
-    @conversations = @mailbox.inbox.paginate(page: params[:page], per_page: 10)
+    @conversations = @mailbox.inbox
   end
 
   private
 
   def get_mailbox
-    @mailbox ||= current_user.mailbox
+    @mailbox ||= current_dreamer.mailbox
+  end
+
+  def get_conversation
+    @conversation ||= @mailbox.conversations.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :dreams
   resources :comments, except: [:show]
+  resources :conversations
   post '/favorite/:dream_id' => 'favorites#create', as: 'favorite'
   delete '/favorite/:dream_id' => 'favorites#destroy', as: 'unfavorite'
 
