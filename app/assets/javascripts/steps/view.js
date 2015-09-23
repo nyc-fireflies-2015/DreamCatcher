06c8fb474c57229c340634e711cb815eb3c6c627
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-23 19:35:52 -0400

    refactor steps view

--- app/assets/javascripts/steps/view.js
+++ app/assets/javascripts/steps/view.js
@@ -1,19 +1,19 @@
-var StepView = {};
-StepView.showNewDreamForm = function(eventTarget){
-  $("[data-form=new_step]").fadeIn("slow");
-  $("[data-link=new_step]").hide();
-  $(".popular-steps-river").hide();
-  $(".popular-title").hide();
+var StepView = {
+  showNewDreamForm: function(eventTarget){
+    $("[data-form=new_step]").fadeIn("slow");
+    $("[data-link=new_step]").hide();
+    $(".popular-steps-river").hide();
+    $(".popular-title").hide();
+  },
+  removeStepDiv: function(eventTarget){
+    $(eventTarget).closest("[data-li=step]").remove();
+  },
+  showNewStep: function(eventTarget, newStep){
+    $("[data-river=recipe_steps]").prepend(newStep);
+    $("[data-form=new_step]").hide();
+    $("[data-form=new_step]").find("form").trigger("reset");
+    $("[data-link=new_step]").fadeIn("slow");
+    $(".popular-steps-river").fadeIn("slow");
+    $(".popular-title").fadeIn("slow");
+  }
 };
-StepView.removeStepDiv = function(eventTarget){
-  $(eventTarget).closest("[data-li=step]").remove();
-};
-StepView.showNewStep = function(eventTarget, newStep){
-  $("[data-river=recipe_steps]").prepend(newStep);
-  $("[data-form=new_step]").hide();
-  $("[data-form=new_step]").find("form").trigger("reset");
-  $("[data-link=new_step]").fadeIn("slow");
-  $(".popular-steps-river").fadeIn("slow");
-  $(".popular-title").fadeIn("slow");
-};
-
