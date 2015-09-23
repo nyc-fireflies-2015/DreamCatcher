170b21cfb783f0980bf313facf500580e8e2ed30
aderend <d.dogrusoy@gmail.com>
2015-09-23 10:48:39 -0400

    clean up

--- app/models/text_worker.rb
+++ app/models/text_worker.rb
@@ -14,7 +14,7 @@ class TextWorker
 
   def perform(number)
     number = number
-    message = 'Poop???'
+    message = 'This is a reality check!'
     twilio_number = ENV["TWILIO_NUMBER"]
     account_sid = ENV["TWILIO_ACCOUNT_SID"]
     auth_token = ENV["TWILIO_AUTH_TOKEN"]
