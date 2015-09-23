616bdad63f6a5d9147807d481a0b73009ed69cb4
aderend <d.dogrusoy@gmail.com>
2015-09-23 09:51:23 -0400

    hid twilio info and call it in the worker

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,2 @@
+twilio_sid = ENV["TWILIO_ACCOUNT_SID"]
+twilio_token = ENV["TWILIO_AUTH_TOKEN"]
--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,3 @@ every 1.minute do
   runner "TextWorker.send_text_to_all"
 end
 
-# , output: '~/Desktop/cron.log'
-
-3475918361
