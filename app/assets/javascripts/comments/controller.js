0248742fef0d87e9c52edd4b56305b5e79f15ed7
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-23 17:28:45 -0400

    remove edit path from comment js

--- app/assets/javascripts/comments/controller.js
+++ app/assets/javascripts/comments/controller.js
@@ -3,12 +3,3 @@ CommentsController.prototype.postComment = function(eventTarget){
     DreamCatcher.send({data: Obj.fromForm({form: eventTarget}),
                       callback: CommentsView.showComment});
 };
-CommentsController.prototype.getCommentForm = function(eventTarget){
-    DreamCatcher.send({callback: CommentsView.showEditForm,
-                      params: eventTarget})
-};
-CommentsController.prototype.updateComment = function(eventTarget){
-    DreamCatcher.send({data: Obj.fromForm({form: eventTarget}),
-                      callback: CommentsView.showSavedComment,
-                      params: eventTarget});
-}
