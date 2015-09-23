2d088dd9052158cd29a46c46a1ff0591a738fc90
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-23 16:30:43 -0400

    Add fadein and reset to comment form

--- app/assets/javascripts/comments/view.js
+++ app/assets/javascripts/comments/view.js
@@ -1,5 +1,8 @@
 var CommentsView = {
   showComment: function(el, newComment){
     $("[data-div=new-comment]").append(newComment);
+    $("[data-form=new-comment]").hide();
+    $("[data-form=new-comment]").trigger("reset");
+    $("[data-form=new-comment]").fadeIn("slow");
   }
 };
