0e36509d49715454d29a937aaaf265c8309dae13
aderend <d.dogrusoy@gmail.com>
2015-09-24 11:20:34 -0400

    Removes precomiplied assets on deploy

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ Bundler.require(*Rails.groups)
 module LucidDreamer
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
