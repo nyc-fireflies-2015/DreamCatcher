d727a9b007491ba847035b9388feedc9d5b3fd91
James Anthony Lowenthal <loweja01@gmail.com>
2015-09-24 20:01:44 -0400

    fix comment destroy

--- app/assets/javascripts/comments/view.js
+++ app/assets/javascripts/comments/view.js
@@ -1,11 +1,23 @@
 var CommentsView = {
   showComment: function(el, newComment){
+    incrementCount("[data-header=comments-count]");
     $("[data-div=new-comment]").append(newComment);
     $("[data-form=new-comment]").hide();
     $("[data-form=new-comment]").trigger("reset");
     $("[data-form=new-comment]").fadeIn("slow");
   },
   removeComment: function(eventTarget){
+    decrementCount("[data-header=comments-count]");
     $(eventTarget).closest("[data-li=destroy-comment]").remove();
   }
 };
+function incrementCount(selector){
+  var val = Number($(selector).text());
+  val += 1;
+  $(selector).text(val.toString());
+}
+function decrementCount(selector){
+  var val = Number($(selector).text());
+  val -= 1;
+  $(selector).text(val.toString());
+}
